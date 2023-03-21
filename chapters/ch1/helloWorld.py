8baedc09f091c48f5a21c826a0b2363611f2cd2b
ashell-git <ashell-git@gmail.com>
2023-03-20 17:50:41 -0700

    initial commits

---
+++ chapters/ch1/helloWorld.py
@@ -0,0 +1,13 @@
+# Ask User for a/n/l
+
+print(f'What is your name?')
+
+myName = input()
+
+print('Hello ' + myName + ' nice to meet you.')
+print('How old are you?')
+
+myAge = input()
+
+print(myName + ' you will be ' + str(int(myAge) + 1) + ' next year. Good bye.')
+
